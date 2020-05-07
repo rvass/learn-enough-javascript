df4548324eda76939fc96cbfd1afcbfb9e4b2158
Richard Vass <rvass@icloud.com>
2020-05-07 18:42:35 +1000

    complete Funtional programming

--- functional.js
+++ functional.js
@@ -33,3 +33,79 @@ function urlAddresses(elements) {
 }
 
 console.log(urlAddresses(states));
+
+
+// 6.2 Filter
+
+// Return array consisting of the strings that have only one word
+// Singles: imperative version
+function imperativeSingles(elements) {
+  let singles = [];
+  elements.forEach(function(element) {
+    if (element.split(/\s+/).length === 1) {
+      singles.push(element);
+    }
+  });
+  return singles;
+}
+console.log(imperativeSingles(states));
+
+// singles: Functional version
+function functionalSingles(elements) {
+  return elements.filter(element => element.split(/\s+/).length === 1);
+}
+console.log(functionalSingles(states));
+
+// 6.2.1 Exercises
+// Write two filter functions that return the Dakotas: one using String#includes (Section 2.5) to test for the presence of the string “Dakota” and one using a regex that tests for the length of the split array being 2
+
+function includesDakota(elements) {
+  return elements.filter(element => element.includes("Dakota"));
+}
+console.log(includesDakota(states));
+  
+function regexDakota(elements) {
+  return elements.filter(element => element.split(/\s+/).length === 2);
+}
+console.log(regexDakota(states));
+
+// 6.3 Reduce
+
+// 6.3.1 Reduce, example 1
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// sum: Imperative solution
+function imperativeSum(elements) {
+  let total = 0;
+  elements.forEach(function(n) {
+    total += n;
+  })
+  return total;
+}
+console.log(imperativeSum(numbers));
+
+// sum: Functional solution
+function functionalSum(elements) {
+  return elements.reduce((total, n) => { return total += n; });
+}
+console.log(functionalSum(numbers));
+
+// lengths: Imperative solution
+function imperativeLengths(elements) {
+  let lengths = {};
+  elements.forEach(function(element) {
+    lengths[element] = element.length;
+  });
+  return lengths;
+}
+console.log(imperativeLengths(states));
+
+// lengths: Functional solution 
+function functionalLengths(elements) {
+  return elements.reduce((lengths, element) => {
+    lengths[element] = element.length;
+    return lengths;
+  }, {});
+}
+console.log(functionalLengths(states));
